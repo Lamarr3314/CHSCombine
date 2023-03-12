7fc26dcabc3c1e0d5d6782116b19890a36fa5a8a
Lamarr3314 <lamarrellis6@gmail.com>
2023-03-11 23:39:34 -0500

    added navbar and html

---
+++ navBar.js
@@ -0,0 +1,29 @@
+let maxWidth = 0;
+let sideWidth = 0;
+let registerDiv = document.getElementById("register");
+let leaderboardDiv = document.getElementById("leaderboard");
+let registerImg = document.getElementById("registerImg");
+let leaderboardImg = document.getElementById("leaderboardImg");
+let mainImg = document.getElementById("mainImage");
+window.addEventListener("load", function () {
+  for (let i = 0; i < 2000; i++) {
+    if (window.matchMedia("(min-width: " + i + "px)").matches) {
+      maxWidth = i;
+    }
+  }
+  console.log(maxWidth);
+  sideWidth = (maxWidth - 101.14) / 2;
+  leaderboardDiv.style.width = sideWidth + "px";
+  registerDiv.style.width = sideWidth + "px";
+  registerDiv.style.marginLeft = (sideWidth - 184.55) / 2 + "px";
+  leaderboardDiv.style.marginLeft = (sideWidth - 200) / 2 + "px";
+});
+registerImg.onclick = function () {
+  window.location.href = "/register.html";
+};
+leaderboardImg.onclick = function () {
+  window.location.href = "/leaders.html";
+};
+mainImg.onclick = function () {
+  window.location.href = "index.html";
+};
