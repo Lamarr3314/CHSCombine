04ab626e32f2054933b548653f7d0a823f40e3b0
Lamarr3314 <lamarrellis6@gmail.com>
2023-03-12 00:03:51 -0500

    beautifulWebsiteNow

--- navBar.js
+++ navBar.js
@@ -1,25 +1,33 @@
-let maxWidth = 0;
+let maxWidth = 1;
 let sideWidth = 0;
 let registerDiv = document.getElementById("register");
 let leaderboardDiv = document.getElementById("leaderboard");
 let registerImg = document.getElementById("registerImg");
 let leaderboardImg = document.getElementById("leaderboardImg");
 let mainImg = document.getElementById("mainImage");
-window.addEventListener("load", function () {
-  for (let i = 0; i < 2000; i++) {
-    if (window.matchMedia("(min-width: " + i + "px)").matches) {
-      maxWidth = i;
-    }
+let maxLastWidth = 0;
+for (let i = 0; i < 2000; i++) {
+  if (window.matchMedia("(min-width: " + i + "px)").matches) {
+    maxWidth = i;
   }
-  console.log(maxWidth);
-  sideWidth = (maxWidth - 101.14) / 2;
-  leaderboardDiv.style.width = sideWidth + "px";
-  registerDiv.style.width = sideWidth + "px";
-  registerDiv.style.marginLeft = (sideWidth - 184.55) / 2 + "px";
-  leaderboardDiv.style.marginLeft = (sideWidth - 200) / 2 + "px";
-  leaderboardImg.style.transform="none";
-  registerImg.style.transform="none";
-});
+}
+console.log(maxWidth);
+sideWidth = (maxWidth - 101.14) / 2;
+leaderboardDiv.style.width = sideWidth + "px";
+registerDiv.style.width = sideWidth + "px";
+registerDiv.style.marginLeft = (sideWidth - 184.55) / 2 + "px";
+leaderboardDiv.style.marginLeft = (sideWidth - 200) / 2 + "px";
+leaderboardImg.style.transform = "none";
+registerImg.style.transform = "none";
+mainImg.style.transform = "none";
+maxLastWidth = maxWidth;
+setTimeout(function() {
+  mainImg.style.display="flex";
+  registerImg.style.display="flex";
+  leaderboardImg.style.display="flex";
+}, .00000001);
+
+
 registerImg.onclick = function () {
   window.location.href = "/register.html";
 };
