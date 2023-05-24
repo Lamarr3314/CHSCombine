b632f9fca1d6228dcaec08b97ed1763e05d73a23
Lamarr3314 <lamarrellis6@gmail.com>
2023-05-24 13:50:23 -0400

    fixed up homepage

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 let introTitle = document.getElementById("introText");
+let innerLogo = document.getElementById("innerLogo");
+let maxHeight;
+let maxWidths;
+let boxWidth = 0;
+let boxHeight = 0;
+let bigTextSize = 0;
+let firstImgSize = 0;
+let boxes = document.querySelectorAll(".glow-on-hover");
+let slideOneImage = document.getElementById("innerLogo");
 var typed = new Typed(".auto-type", {
   strings: ["CHS Combine"],
   typeSpeed: 150,
@@ -7,9 +16,36 @@ var typed = new Typed(".auto-type", {
   startDelay: 2500,
   showCursor: true,
   onComplete: (self) => {
-    setTimeout(function () {
-      //   self.cursor.remove();
-      delete self;
-    }, 1300);
+    delete self;
   },
+});
+
+window.addEventListener("load", function () {
+  for (let i = 0; i < 10000; i++) {
+    if (window.matchMedia("(min-width: " + i + "px)").matches) {
+      maxWidths = i;
+    }
+  }
+  for (let i = 0; i < 10000; i++) {
+    if (window.matchMedia("(min-height: " + i + "px)").matches) {
+      maxHeight = i;
+    }
+  }
+  setAssets();
+  function setAssets() {
+    boxWidth = maxWidths * 0.94;
+    boxHeight = maxHeight * 0.7;
+    bigTextSize = maxHeight * 0.1;
+    firstImgSize = maxWidths * 0.2;
+    if (boxWidth < 600) {
+      firstImgSize = boxWidth * 0.45;
+    }
+    for (let i = 0; i < boxes.length; i++) {
+      boxes[i].style.width = boxWidth + "px";
+      boxes[i].style.height = boxHeight + "px";
+      slideOneImage.style.width = firstImgSize + "px";
+      slideOneImage.style.marginLeft = (boxWidth - firstImgSize) / 2 + "px";
+      document.getElementById("introText").style.fontSize = bigTextSize + "px";
+    }
+  }
 });
